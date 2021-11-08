ba30f647d60b81f8ee7638be028cc58a60dc5015
pipranja <pipranja@cisco.com>
2021-11-08 22:09:52 +0530

    css added

--- src/calc/calcView.js
+++ src/calc/calcView.js
@@ -8,28 +8,48 @@ class CalcView extends Component {
         const {concatHandler,clearScr,backspace,display,result} = this.props
 
         return (
-            <div>
-                <input type = "text" value = {display}/>
-                <button name = "1" onClick = {concatHandler}>1</button>
-                <button name = "2" onClick = {concatHandler}>2</button>
-                <button name = "3" onClick = {concatHandler}>3</button>
-                <button name = "4" onClick = {concatHandler}>4</button>
-                <button name = "5" onClick = {concatHandler}>5</button>
-                <button name = "6" onClick = {concatHandler}>6</button>
-                <button name = "7" onClick = {concatHandler}>7</button>
-                <button name = "8" onClick = {concatHandler}>8</button>
-                <button name = "9" onClick = {concatHandler}>9</button>
-                <button name = "0" onClick = {concatHandler}>0</button>
-                <button name = "+" onClick = {concatHandler}>+</button>
-                <button name = "-" onClick = {concatHandler}>-</button>
-                <button name = "*" onClick = {concatHandler}>x</button>
-                <button name = "/" onClick = {concatHandler}>&divide;</button>
-                <button name = "." onClick = {concatHandler}>.</button>
-                <button onClick = {result}>=</button>
-                <button onClick = {clearScr}>Clear</button>
-                <button onClick = {backspace}>C</button>
+            <div className = "app">
+                    <div className = "wrapper">
+                    <input type = "text" value = {display}/>
+
+                    <div className = "row">
+                        <button name = "1" onClick = {concatHandler}>1</button>
+                        <button name = "2" onClick = {concatHandler}>2</button>
+                        <button name = "3" onClick = {concatHandler}>3</button>
+                        <button name = "+" onClick = {concatHandler}>+</button>
+                    </div>
+
+                    <div className = "row">
+                        <button name = "4" onClick = {concatHandler}>4</button>
+                        <button name = "5" onClick = {concatHandler}>5</button>
+                        <button name = "6" onClick = {concatHandler}>6</button>
+                        <button name = "-" onClick = {concatHandler}>-</button>
+                    </div>
+
+                    <div className = "row">
+                        <button name = "7" onClick = {concatHandler}>7</button>
+                        <button name = "8" onClick = {concatHandler}>8</button>
+                        <button name = "9" onClick = {concatHandler}>9</button>
+                        <button name = "*" onClick = {concatHandler}>x</button>
+
+                    </div>
+                    
+                    <div className = "row">
+                        <button name = "." onClick = {concatHandler}>.</button>
+                        <button name = "0" onClick = {concatHandler}>0</button>
+                        <button onClick = {result}>=</button>
+                        <button name = "/" onClick = {concatHandler}>&divide;</button>
+                    </div>
+
+                    <div className = "row">
+                        <button onClick = {clearScr}>Clear</button>
+                        <button onClick = {backspace}>C</button>
+                    </div>                   
             
+                </div>
             </div>
+
+            
         )
     }
 }
